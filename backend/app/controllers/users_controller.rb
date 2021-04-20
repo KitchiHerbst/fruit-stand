13f16bc5a548cdb51ad3c9d50567c3d027ff6441
Jose Mendoza <pball626@gmail.com>
2021-04-20 14:56:53 -0500

    backend name created

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -3,4 +3,18 @@ class UsersController < ApplicationController
         user = User.all
         render json: user
     end
+
+    def new
+        user = User.create
+        render json: user
+    end
+
+    def show
+        user = User.find()
+    end
+
+    def create
+        user = User.create(name: params[:name])
+        render json: user
+    end
 end
