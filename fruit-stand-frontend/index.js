bace2387f5c4c47f2fcc5f6fce46192255c93c67
Jose Mendoza <pball626@gmail.com>
2021-04-20 17:40:40 -0500

    end of day jose

--- fruit-stand-frontend/index.js
+++ fruit-stand-frontend/index.js
@@ -21,5 +21,11 @@ const loadForm = (e) => {
         })
         .then(res => res.json())
         .then(newPlayer => console.log(newPlayer))
+        //create function that verifies if user is already in game or not
     })
 }
+
+// 2 forms log in and sign up 
+// sign up creates user and signs in
+// log in needs to be matched to database names (interpolate with name given to match object in db)
+// make play
