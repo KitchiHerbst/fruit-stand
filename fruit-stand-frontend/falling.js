6e36a3696d217408e3a90e587789d1007643955d
Triston <tristonherbst97@gmail.com>
2021-04-20 11:53:27 -0500

    added fruits and bug

--- fruit-stand-frontend/falling.js
+++ fruit-stand-frontend/falling.js
@@ -1,14 +1,27 @@
+//creating the canvas element so we can have moving objects on it
 let canvas = document.getElementById('canvas')
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
-    
-let context = canvas.getContext('2d')
-let img = document.createElement('img')
-img.src = '5a0214fe18e87004f1ca4383.png'
 
-let numOfFruit = 30
-let fruits = []
-    
+//setting the context to be 2d
+let context = canvas.getContext('2d')
+
+//creating the images for the fruits
+let bananaImg = document.createElement('img')
+bananaImg.src = 'banana.png'
+let blueberryImg = document.createElement('img')
+blueberryImg.src = 'blueberry.png'
+let strawberryImg = document.createElement('img')
+strawberryImg.src = 'strawberry.png'
+
+let numOfObjects = 30
+let objects = []
+// let numOfFruit = 30
+// let fruits = []
+
+//creating an array so that i can select a random fruit image when creating a new fruit
+let imgArray = [bananaImg,blueberryImg,strawberryImg]
+// creating the Fruit class so i can call on it later to create any number of fruits that i need
 function Fruit(x,y) {
     this.x = x
     this.y = y
@@ -17,28 +30,60 @@ function Fruit(x,y) {
         this.y = this.y+1
         if(this.y > canvas.height){
             this.y = 0
+            // objects.filter(fruit => fruit !== this)
+        }
+    }
+    //should get me a random number between 0 - 2
+    let randomNumber = Math.floor(Math.random()*imgArray.length)
+    // need to change img to be a random image between the 3 fruits
+    this.show = function(){
+        context.drawImage(imgArray[randomNumber], this.x, this.y, 50, 50)
+    }
+}
+
+//creating the image for the bug
+let bugImg = document.createElement('img')
+bugImg.src = 'bug1.png'
+
+function Bug(x,y) {
+    this.x = x
+    this.y = y
+
+    this.fall = function(){
+        this.y = this.y+1
+        if(this.y > canvas.height){
+            this.y = 0
+            // objects.filter(bug => bug !== this)
         }
     }
     
+    
     this.show = function(){
-        context.drawImage(img, this.x, this.y, 20, 20)
+        context.drawImage(bugImg, this.x, this.y, 100, 100)
     }
 }
 
 
-for (let i=0;i < numOfFruit; i++){
+
+
+for (let i=0;i < numOfObjects; i++){
     let x = Math.floor(Math.random()*canvas.width)
     let y = Math.floor(Math.random()*canvas.height)
-    fruits[i] = new Fruit(x,y)
+    let randNum = Math.floor(Math.random()*4)
+    if(randNum == 1){
+        objects[i] = new Bug(x,y)
+    }else{
+        objects[i] = new Fruit(x,y)
+    }
 }
 
 
 const draw = () => {
     context.fillStyle = 'black'
     context.fillRect(0, 0, canvas.width, canvas.height)
-    for(let i=0;i < numOfFruit; i++){
-        fruits[i].show()
-        fruits[i].fall()
+    for(let i=0;i < numOfObjects; i++){
+        objects[i].show()
+        objects[i].fall()
     }
 }
 
