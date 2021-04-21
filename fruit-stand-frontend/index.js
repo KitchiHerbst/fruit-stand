d8445e353bb3fc24dc32fb4ac6bb92701f9fdfcd
Jose Mendoza <pball626@gmail.com>
2021-04-21 16:19:55 -0500

    headers renamed

--- fruit-stand-frontend/index.js
+++ fruit-stand-frontend/index.js
@@ -5,6 +5,7 @@
 
 const loadForm = () => {
     let nameForm = document.querySelector('.new-player-form')
+    let existingNameForm = document.querySelector('.existing-player')
     let errorElement = document.getElementById('error')
     nameForm.addEventListener('submit', (e) => {
         e.preventDefault()
@@ -29,14 +30,65 @@ const loadForm = () => {
             })
         })
         .then(res => res.json())
-        .then(newPlayer => console.log(newPlayer))
-        //create function that verifies if user is already in game or not
+        .then(newPlayer => renderPlayerCard(newPlayer))
+        //     //create function that verifies if user is already in game or not
+        // })
     })
+    const loadNewPlayer = () =>{
+        let nameForm = document.querySelector('.new-player-form')
+        let errorElement = document.getElementById('error')
+        nameForm.addEventListener('submit', (e) => {
+            e.preventDefault()
+            let messages = []
+            let newName = e.target.name.value
+            if (newName.value === '' || newName.value == null) {
+                messages.push('Name is required')
+            }
+            if (messages.length > 0) {
+                e.preventDefault()
+                errorElement.innerText = messages.join(', ')
+            }
+            // console.log(newName)
+            fetch('http://localhost:3000/users', {
+                method: 'POST',
+                headers: {
+                    'Content-Type':'application/json',
+                    'Accept':'application/json'
+                },
+                body: JSON.stringify({
+                    name: newName
+                })
+            })
+            .then(res => res.json())
+            .then(newPlayer => renderPlayerCard(newPlayer.id))
+            //create function that verifies if user is already in game or not
+            })
+        }
+    }
     
+
+const renderPlayerCard = (newPlayer) => {
+    let div = document.querySelector('.column')
+    // div.className = 'card p-2 m-2'
+    let container = document.querySelector('.container')
+    container.innerHTML = ''
+    
+    let playerHeader = document.createElement('h2')
+    playerHeader.innerHTML = newPlayer.name
+
+    let playerScore = document.createElement('p')
+    playerScore.innerHTML = newPlayer.score
+    // debugger
+    container.append(playerHeader, playerScore)
+    // console.log(newPlayer)
 }
+
+
+//create header for player name 
+//create area for high score
+//append all these attributes to container card to be displayed
 
 
 // 2 forms log in and sign up 
 // sign up creates user and signs in
 // log in needs to be matched to database names (interpolate with name given to match object in db)
-// make play
--- fruit-stand-frontend/landing-page.js
+++ fruit-stand-frontend/landing-page.js
@@ -12,54 +12,58 @@ let head = document.getElementById('head')
 body.innerHTML = 
 `<div class = 'row'>
         <div class = 'column'>
-        <div id='error'></div>
-            <div class='container'>
-                <form class='new-player-form'>
-                    <h3>Enter Your Name</h3>
-                    <input
-                    type='text'
-                    name='name'
-                    value=''
-                    placeholder='Enter Name Here'
-                    class='input-text'
-                    />
-                    <br />
-                    <input
-                    type='submit'
-                    name='submit'
-                    value='Les Gooo'
-                    class='submit'
-                    />
-                    </form>
-                    <br>
-                    <form class='Existing-Player'>
-                    <h3>Existing Player/h3>
-                    <input
-                    type='text'
-                    name='name'
-                    value=''
-                    placeholder='Enter Name Here'
-                    class='input-text'
-                    />
-                    <br />
-                    <input
-                    type='submit'
-                    name='submit'
-                    value='Les Gooo'
-                    class='submit'
+            <h3> Players Info </h3>
+                <div id='error'></div>
+                    <div class='container'>
+                        <form class='new-player-form'>
+                        <h3>Enter New Name</h3>
+                        <input
+                        type='text'
+                        name='name'
+                        value=''
+                        placeholder='Enter Name Here'
+                        class='input-text'
+                        />
+                        <br />
+                        <input
+                        type='submit'
+                        name='submit'
+                        value='Les Gooo'
+                        class='submit'
+                        />
+                        </form>
+                        <br>
+                        <form class='existing-player'>
+                        <h3>Existing Player</h3>
+                        <input
+                        type='text'
+                        name='name'
+                        value=''
+                        placeholder='Enter Name Here'
+                        class='input-text'
+                        />
+                        <br />
+                        <input
+                        type='submit'
+                        name='submit'
+                        value='Les Gooo'
+                        class='submit'
                     />
                 </form>
             </div>
         </div>
+        
         <div class = 'column'>
-            <h3>Column 2</h3>
+            <h3>Fruit Catch</h3>
             <button id='game-button'>Play Game</button>
         </div>
+        
         <div class = 'column'>
-            <h3>Column 3</h3>
+            <h3>High Scores</h3>
         </div>
     </div>`
     loadForm()
+    // loadNewPlayer()
 
 let gameButton = document.getElementById('game-button')
 gameButton.addEventListener('click', () => {
