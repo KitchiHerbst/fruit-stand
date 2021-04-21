da470ed12d02b45f21cd0bbc24ded7365e0d575b
Jose Mendoza <pball626@gmail.com>
2021-04-21 11:18:11 -0500

    new player form edits

--- fruit-stand-frontend/index.js
+++ fruit-stand-frontend/index.js
@@ -5,9 +5,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const loadForm = (e) => {
     let nameForm = document.querySelector('.new-player-form')
+    let errorElement = document.getElementById('error')
     nameForm.addEventListener('submit', (e) => {
         e.preventDefault()
+        let messages = []
         let newName = e.target.name.value
+        if (newName.value === '' || newName.value == null) {
+            messages.push('Name is required')
+        }
+        if (messages.length > 0) {
+            e.preventDefault()
+            errorElement.innerText = messages.join(', ')
+        }
         console.log(newName)
         fetch('http://localhost:3000/users', {
             method: 'POST',
@@ -23,7 +32,9 @@ const loadForm = (e) => {
         .then(newPlayer => console.log(newPlayer))
         //create function that verifies if user is already in game or not
     })
+    
 }
+
 
 // 2 forms log in and sign up 
 // sign up creates user and signs in
