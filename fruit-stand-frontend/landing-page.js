aa161fa8c89fa7d5b5dab65d37f1b54a3ffc09b3
github username <github email address>
2021-04-22 09:33:42 -0500

    css changes and user credentials show

--- fruit-stand-frontend/landing-page.js
+++ fruit-stand-frontend/landing-page.js
@@ -7,8 +7,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const load = (user) => {let body = document.getElementById('body')
 // let head = document.getElementById('head')
+    body.className = 'body'
     body.innerHTML =` 
         <div class = 'row'>
+            <div class = 'column'>
+                <h3>High Scores</h3>
+            </div>
+            <div class = 'column'>
+                <h3>Fruit Catch</h3>
+                <button id='game-button'>Play Game</button>
+            </div>
             <div class = 'column'>
                 <h3> Players Info </h3>
                     <div id='error'></div>
@@ -50,21 +58,16 @@ const load = (user) => {let body = document.getElementById('body')
                             </form>
                         </div>
                     </div>
-                
-                <div class = 'column'>
-                    <h3>Fruit Catch</h3>
-                    <button id='game-button'>Play Game</button>
-                </div>
-                
-                <div class = 'column'>
-                    <h3>High Scores</h3>
                 </div>
             </div>
         </div>
     `
-    loadForm(user)
+    loadForm()
+    // loadForm defined on index.js
+}
 
-
-
+const showScorePage = () => {
+    body.innerHTML = ''
+    body.innerHTML = `<h1>Winner!</h1>`
 }
 
